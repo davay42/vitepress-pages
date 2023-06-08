ab16f4f1520c2cf14f10b3f678a67e1850ecc018
davay <davay@defucc.me>
2023-06-08 14:00:41 +0700

    conditional head

--- src/head.ts
+++ src/head.ts
@@ -21,37 +21,37 @@ export default function (metaData: {
     const url = ctx.pageData.relativePath.split('index.md')[0]
     let image = metaData?.image
     if (ctx.pageData.frontmatter?.cover) {
-      image = `${metaData.mediaFolder}/cover/${url.split('/').join('-') + ctx.pageData.frontmatter?.cover}`
+      image = `${metaData.mediaFolder || 'media_files'}/cover/${url.split('/').join('-') + ctx.pageData.frontmatter?.cover}`
     }
     return [
 
-      process.env.NODE_ENV === "production" && metaData.umamiId ? ["script", { async: true, defer: true, "data-website-id": metaData.umamiId, src: metaData.umamiScript }] : null,
+      process.env.NODE_ENV === "production" && metaData.umamiScript && metaData.umamiId ? ["script", { async: true, defer: true, "data-website-id": metaData.umamiId, src: metaData.umamiScript }] : null,
 
-      ["link", { rel: "icon", type: "image/svg+xml", href: metaData.icon }],
+      metaData.icon ? ["link", { rel: "icon", type: "image/svg+xml", href: metaData.icon }] : null,
+      metaData?.author ? ["meta", { name: "author", content: metaData?.author }] : null,
+      metaData?.tags ? ["meta", { name: "keywords", content: metaData?.tags }] : null,
+      metaData.color ? ["meta", { name: "theme-color", content: metaData.color }] : null,
 
       ["meta", { property: "og:type", content: "website" }],
-      ["meta", { property: "og:site", content: metaData.site }],
-      ["meta", { property: "og:site_name", content: metaData.title }],
+      metaData.site ? ["meta", { property: "og:site", content: metaData.site }] : null,
+      metaData.title ? ["meta", { property: "og:site_name", content: metaData.title }] : null,
       ['meta', { property: 'og:title', content: `${ctx.pageData.title} | ${metaData.title}` }],
       ['meta', { property: 'og:description', content: ctx.pageData.description }],
-      ['meta', { property: 'og:url', content: metaData.url + url }],
-      ['meta', { property: 'og:image', content: metaData.url + image }],
-      ["meta", { property: "og:locale", content: metaData.locale }],
+      metaData.url ? ['meta', { property: 'og:url', content: metaData.url + url }] : null,
+      image && ['meta', { property: 'og:image', content: metaData.url + image }],
+      metaData.locale ? ["meta", { property: "og:locale", content: metaData.locale }] : null,
 
-      ['meta', { name: 'twitter:title', content: `${ctx.pageData.title} | ${metaData.title}` }],
+      metaData.title ? ['meta', { name: 'twitter:title', content: `${ctx.pageData.title} | ${metaData.title}` }] : null,
       ['meta', { name: 'twitter:description', content: ctx.pageData.description }],
       ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-      ['meta', { name: 'twitter:site', content: `@${metaData.author}` }],
-      ['meta', { name: 'twitter:creator', content: `@${metaData.author}` }],
-      ['meta', { name: 'twitter:image', content: metaData.url + image }],
+      metaData?.author ? ['meta', { name: 'twitter:site', content: `@${metaData.author}` }] : null,
+      metaData?.author ? ['meta', { name: 'twitter:creator', content: `@${metaData.author}` }] : null,
+      image ? ['meta', { name: 'twitter:image', content: metaData.url + image }] : null,
 
-      ["meta", { name: "author", content: metaData?.author }],
-      ["meta", { name: "keywords", content: metaData?.tags }],
       ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "white-translucent", },],
       ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
       ["meta", { name: "HandheldFriendly", content: "True" }],
       ["meta", { name: "MobileOptimized", content: "320" }],
-      ["meta", { name: "theme-color", content: metaData.color }],
-    ]
+    ].filter(Boolean)
   }
 }
